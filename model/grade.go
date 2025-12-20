1170285b0620bfa48e86d7810da9208ba257e583
arvinpaundra <arvinpaundra06@gmail.com>
2025-12-20 23:08:28 +0700

    feat: add subject and grade models

---
+++ model/grade.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/guregu/null/v6"
+)
+
+type Grade struct {
+	ID          uuid.UUID   `gorm:"primaryKey;column:id"`
+	Name        string      `gorm:"column:name"`
+	Description null.String `gorm:"nullable;column:description"`
+	CreatedAt   time.Time   `gorm:"column:created_at"`
+	UpdatedAt   time.Time   `gorm:"column:updated_at"`
+	DeletedAt   null.Time   `gorm:"nullable;column:deleted_at"`
+}
